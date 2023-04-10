0cc0277c3dea33ec865c658592b1bcecff82c413
ericksantos2 <ericksantos2@github.com>
2023-04-10 14:20:12 -0300

    adicionei o provider do redux no root do projeto

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import 'reset.css'
 import './styles/index.css'
 import Router from './routes'
+import store from './store'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </React.StrictMode>,
 )
