2a211df833b69e461fb91f7d3fbf6bf079aa1dee
ericksantos2 <ericksantos2@github.com>
2023-04-10 14:38:43 -0300

    adicionei os itens da categoria da pagina atual

--- src/pages/Categoria/index.tsx
+++ src/pages/Categoria/index.tsx
@@ -3,16 +3,24 @@ import Header from '../../components/Header';
 import { useParams } from 'react-router-dom';
 import { RootState } from '../../store';
 import { ICategoria } from '../../store/reducers/categorias';
+import { Iitem } from '../../store/reducers/itens';
+import styles from './Categoria.module.scss';
 
 export default function Categoria() {
   const { nomeCategoria } = useParams();
-  const categoria: ICategoria | undefined = useSelector((state: RootState) =>
-    state.categorias.find((categoria) => categoria.id === nomeCategoria)
-  );
 
-  if (!categoria) {
-    throw new Error('Categoria não encontrada');
-  }
+  const {
+    categoria,
+    itens,
+  }: { categoria: ICategoria | undefined; itens: Iitem[] } = useSelector(
+    (state: RootState) => ({
+      categoria: state.categorias.find(
+        (categoria) => categoria.id === nomeCategoria
+      ),
+      itens: state.itens.filter((item) => item.categoria === nomeCategoria),
+    })
+  );
+  if (!categoria) return;
 
   return (
     <div>
@@ -21,6 +29,13 @@ export default function Categoria() {
         descricao={categoria.descricao}
         imagem={categoria.header}
       />
+      <div className={styles.itens}>
+        {itens?.map((item: Iitem) => (
+          <div key={item.id}>
+            {item.titulo}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
