25afdef231b89b454c0f43c5254b9fd8392862df
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-04-20 10:03:50 -0300

    adicionei as funções de mudar quantidade e de resetar o carrinho

--- src/store/reducers/carrinho.tsx
+++ src/store/reducers/carrinho.tsx
@@ -9,18 +9,35 @@ const carrinhoSlice = createSlice({
   reducers: {
     mudarCarrinho: (state, { payload }) => {
       const temItem = state.some((item: Iitem) => item.id === payload);
-      if (!temItem) return [
-        ...state,
-        {
-          id: payload,
-          quantidade: 1
-        }
-      ]
+      if (!temItem)
+        return [
+          ...state,
+          {
+            id: payload,
+            quantidade: 1,
+          },
+        ];
       return state.filter((item: Iitem) => item.id !== payload);
-    }
-  }
-})
+    },
+    mudarQuantidade: (
+      state: Iitem[],
+      { payload }: { payload: { id: string; quantidade: number } }
+    ) => {
+      state = state.map((itemNoCarrinho) => {
+        if (itemNoCarrinho.id === payload.id) {
+          if (!itemNoCarrinho.quantidade) {
+            itemNoCarrinho.quantidade = 0;
+          }
+          itemNoCarrinho.quantidade += payload.quantidade;
+        }
+        return itemNoCarrinho;
+      });
+    },
+    resetarCarrinho: () => initialState,
+  },
+});
 
-export const { mudarCarrinho } = carrinhoSlice.actions;
+export const { mudarCarrinho, mudarQuantidade, resetarCarrinho } =
+  carrinhoSlice.actions;
 
 export default carrinhoSlice.reducer;
