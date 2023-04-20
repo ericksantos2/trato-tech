f31ee41b2ecdf4ef9f13d647e1a6d2559d7fd4a1
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-04-20 10:00:26 -0300

    adicionei o texto da busca ao redux

--- src/components/Busca/index.tsx
+++ src/components/Busca/index.tsx
@@ -1,9 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './Busca.module.scss';
+import { RootState } from '../../store';
+import { mudarBusca, resetarBusca } from '../../store/reducers/busca';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function Busca() {
+  const dispatch = useDispatch();
+  const busca = useSelector((state: RootState) => state.busca);
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(resetarBusca());
+  }, [location.pathname, dispatch])
+  
   return (
     <div className={styles.busca}>
-      <input className={styles.input} placeholder='O que você procura?' />
+      <input
+        className={styles.input}
+        placeholder='O que você procura?'
+        value={busca}
+        onChange={(e) => dispatch(mudarBusca(e.target.value))}
+      />
     </div>
   );
 }
