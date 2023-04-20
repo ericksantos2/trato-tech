3239bdb882f46d2b94ca78c303777c89c359c7c2
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-04-20 10:01:52 -0300

    adicionei o subtotal e a busca de itens na página de carrinho

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,27 +1,40 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Header from '../../components/Header';
 import styles from './Carrinho.module.scss';
 import { RootState } from '../../store';
 import Item from '../../components/Item';
 import { Iitem } from '../../store/reducers/itens';
+import { resetarCarrinho } from '../../store/reducers/carrinho';
 
 export default function Carrinho() {
-  const carrinho = useSelector((state: RootState) => {
+  const dispatch = useDispatch();
+  const { carrinho, total } = useSelector((state: RootState) => {
+    let total = 0;
+    const regexp = new RegExp(state.busca, 'i');
     const carrinhoReduce = state.carrinho.reduce(
       (itens: Iitem[], itemNoCarrinho: Iitem) => {
         const item = state.itens.find((item) => item.id === itemNoCarrinho.id);
-        if (!item) {
+        if (!item || !item.preco) {
           throw new Error('Item não encontrado');
         }
-        itens.push({
-          ...item,
-          quantidade: itemNoCarrinho.quantidade,
-        });
+        if (itemNoCarrinho.quantidade === undefined) {
+          itemNoCarrinho.quantidade = 0;
+        }
+        total += item.preco * itemNoCarrinho.quantidade;
+        if (item.titulo?.match(regexp)) {
+          itens.push({
+            ...item,
+            quantidade: itemNoCarrinho.quantidade,
+          });
+        }
         return itens;
       },
       []
     );
-    return carrinhoReduce;
+    return {
+      carrinho: carrinhoReduce,
+      total,
+    };
   });
   return (
     <div>
@@ -35,8 +48,16 @@ export default function Carrinho() {
         ))}
         <div className={styles.total}>
           <strong>Resumo da compra</strong>
-          <span>Subtotal: <strong> R$ {0.0.toFixed(2)} </strong></span>
+          <span>
+            Subtotal: <strong> R$ {total.toFixed(2)} </strong>
+          </span>
         </div>
+        <button
+          className={styles.finalizar}
+          onClick={() => dispatch(resetarCarrinho())}
+        >
+          Finalizar compra
+        </button>
       </div>
     </div>
   );
