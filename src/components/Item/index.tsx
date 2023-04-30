f1f5b857a35df7fbbbb2a5facc702f4ccad2a3f8
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-04-30 12:37:38 -0300

    adicionei um botão para editar o item

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,16 +1,20 @@
-import { Iitem, mudarFavorito } from '../../store/reducers/itens';
+import { Iitem, mudarFavorito, mudarItem } from '../../store/reducers/itens';
 import styles from './Item.module.scss';
 import {
   AiOutlineHeart,
   AiFillHeart,
   AiFillMinusCircle,
   AiFillPlusCircle,
+  AiOutlineCheck,
+  AiFillEdit,
 } from 'react-icons/ai';
 import { FaCartPlus } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import { mudarCarrinho, mudarQuantidade } from '../../store/reducers/carrinho';
 import { RootState } from '../../store';
 import classNames from 'classnames';
+import { useState } from 'react';
+import Input from '../Input';
 
 const iconeProps = {
   size: 24,
@@ -32,6 +36,8 @@ export default function Item(props: Props) {
   if (!preco) {
     throw new Error('há props necessárias que não foram passadas');
   }
+  const [modoDeEdicao, setModoDeEdicao] = useState(false);
+  const [novoTitulo, setNovoTitulo] = useState(titulo || '');
   const dispatch = useDispatch();
   const estaNoCarrinho = useSelector((state: RootState) =>
     state.carrinho.some((itemNoCarrinho: Iitem) => itemNoCarrinho.id === id)
@@ -45,6 +51,32 @@ export default function Item(props: Props) {
     dispatch(mudarCarrinho(id));
   }
 
+  const componenteModoDeEdicao = (
+    <>
+      {modoDeEdicao ? (
+        <AiOutlineCheck
+          {...iconeProps}
+          className={styles['item-acao']}
+          onClick={() => {
+            setModoDeEdicao(false);
+            dispatch(
+              mudarItem({
+                id,
+                item: { titulo: novoTitulo },
+              })
+            );
+          }}
+        />
+      ) : (
+        <AiFillEdit
+          {...iconeProps}
+          className={styles['item-acao']}
+          onClick={() => setModoDeEdicao(true)}
+        />
+      )}
+    </>
+  );
+
   return (
     <div
       className={classNames(styles.item, {
@@ -56,7 +88,14 @@ export default function Item(props: Props) {
       </div>
       <div className={styles['item-descricao']}>
         <div className={styles['item-titulo']}>
-          <h2>{titulo}</h2>
+          {modoDeEdicao ? (
+            <Input
+              value={novoTitulo}
+              onChange={(e) => setNovoTitulo(e.target.value)}
+            />
+          ) : (
+            <h2>{titulo}</h2>
+          )}
           <p>{descricao}</p>
         </div>
         <div className={styles['item-info']}>
@@ -79,10 +118,13 @@ export default function Item(props: Props) {
             {carrinho ? (
               <div className={styles.quantidade}>
                 Quantidade:
-                <AiFillMinusCircle {...quantidadeProps} onClick={() => {
-                  if (quantidade === 0) return;
-                  dispatch(mudarQuantidade({ id, quantidade: -1 }))
-                }} />
+                <AiFillMinusCircle
+                  {...quantidadeProps}
+                  onClick={() => {
+                    if (quantidade === 0) return;
+                    dispatch(mudarQuantidade({ id, quantidade: -1 }));
+                  }}
+                />
                 <span>{String(quantidade || 0).padStart(2, '0')}</span>
                 <AiFillPlusCircle
                   {...quantidadeProps}
@@ -92,12 +134,15 @@ export default function Item(props: Props) {
                 />
               </div>
             ) : (
-              <FaCartPlus
-                {...iconeProps}
-                color={estaNoCarrinho ? '#1875E8' : iconeProps.color}
-                className={styles['item-acao']}
-                onClick={resolverCarrinho}
-              />
+              <>
+                <FaCartPlus
+                  {...iconeProps}
+                  color={estaNoCarrinho ? '#1875E8' : iconeProps.color}
+                  className={styles['item-acao']}
+                  onClick={resolverCarrinho}
+                />
+                {componenteModoDeEdicao}
+              </>
             )}
           </div>
         </div>
