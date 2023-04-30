e79e1f77129a078e1b0030f11b31fb144c7c8e2f
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-04-30 12:36:30 -0300

    adicionei a prop children

--- src/components/Header/TituloSemImagem/index.tsx
+++ src/components/Header/TituloSemImagem/index.tsx
@@ -3,14 +3,17 @@ import styles from './TituloSemImagem.module.scss';
 export default function TituloSemImagem({
   titulo,
   descricao,
+  children,
 }: {
   titulo: string;
   descricao: string;
+  children?: any;
 }) {
   return (
     <div className={styles.container}>
       <h1 className={styles.titulo}>{titulo}</h1>
       <h2 className={styles.descricao}>{descricao}</h2>
+      {children}
     </div>
   );
 }
