54b28467f3d81ad1a61cb21de11ac42864c708bc
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-05-05 11:56:06 -0300

    adicionei o toast e comentei o strictmode pois não aparecia nada ao usar ele com o toast

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,15 @@ import './styles/index.css'
 import Router from './routes'
 import store from './store'
 import { Provider } from 'react-redux'
+import { createStandaloneToast } from '@chakra-ui/toast';
+
+const { ToastContainer, toast } = createStandaloneToast();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Provider store={store}>
       <Router />
+      <ToastContainer />
     </Provider>
-  </React.StrictMode>,
+  // </React.StrictMode>
 )
